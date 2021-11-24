e19dcee73ae9a5a82473d57e395fbe2afc22d2b0
LMH01 <louis.haensch01@gmail.com>
2021-11-24 20:55:55 +0100

    added keybinds to nvim

---
+++ .config/nvim/lua/maps.lua
@@ -0,0 +1,12 @@
+map = vim.api.nvim_set_keymap
+
+-- Use <c-space> to trigger completion.
+map("i", "<c-space>", "coc#refresh()", { silent = true, expr = true })
+-- symbol renaming
+map("n", "-n", "<Plug>(coc-rename)", { silent = true })
+-- symbol renaming
+map("n", "-n", "<Plug>(coc-rename)", { silent = true })
+-- apply AutoFix to problem on current line
+map("n", "-f", "<Plug>(coc-fix-current)", { silent = true })
+-- open action dialog
+map("n", "-a", ":CocAction<CR>", { silent = true })
