2626272082733068b78dead932257b795ec80712
LMH01 <louis.haensch01@gmail.com>
2022-07-03 23:56:25 +0200

    updated fish config to include zoxide and print colored man pages

--- general/.config/fish/config.fish
+++ general/.config/fish/config.fish
@@ -1,1 +1,18 @@
-starship init fish | source
+# colored man pages
+set -gx LESS_TERMCAP_mb \e'[1;32m'
+set -gx LESS_TERMCAP_md \e'[1;32m'
+set -gx LESS_TERMCAP_me \e'[0m'
+set -gx LESS_TERMCAP_se \e'[0m'
+set -gx LESS_TERMCAP_so \e'[01;33m'
+set -gx LESS_TERMCAP_ue \e'[0m'
+set -gx LESS_TERMCAP_us \e'[1;4;31m'
+
+function ifpresent
+    if which $argv[1] &>/dev/null
+        eval $argv[2..-1]
+    end
+end
+
+# initialization stuff
+ifpresent starship 'starship init fish | source'
+ifpresent zoxide 'zoxide init fish | source'
